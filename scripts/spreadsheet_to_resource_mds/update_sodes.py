036a78441dae79026b24db294b4cd292d3c274c0
Gigi <dergigi@pm.me>
2020-05-22 13:47:05 +0200

    Sodes -> Episodes

--- scripts/spreadsheet_to_resource_mds/update_sodes.py
+++ scripts/spreadsheet_to_resource_mds/update_sodes.py
@@ -12,7 +12,7 @@ scope = ['https://spreadsheets.google.com/feeds',
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
-sodes = client.open("Bitcoin Resources").worksheet("Sodes")
+sodes = client.open("Bitcoin Resources").worksheet("Episodes")
 
 NO_DATE = "1111-11-11"
 NO_AUTHOR_LINKS = ""
