7d9ec16e96f090a6a7a3e0eb6c137ad21a23a207
Gigi <dergigi@pm.me>
2020-10-30 23:22:34 +0100

    chore: conventional commits in update deps script

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 bundle update --bundler
 git add .
-git commit -m "bundle update --bundler"
+git commit -m "build: bundle update --bundler"
 
 ncu -u
 git add .
-git commit -m "ncu -u"
+git commit -m "build: ncu -u"
 
 npm install
 git add .
-git commit -m "npm install"
+git commit -m "build: npm install"
 
 npm audit fix
 git add .
-git commit -m "npm audit fix"
+git commit -m "build: npm audit fix"
 
 bundle update
 git add .
-git commit -m "bundle update"
+git commit -m "build: bundle update"
