7defd96f7e65e334831175e5e3350dc87a0c4de1
CTNBTC <me@ctnbtc.bar>
2021-12-04 16:27:38 +0100

    minor changes to archiving script

--- scripts/archive_links_from_spreadsheet/archive_article_links.py
+++ scripts/archive_links_from_spreadsheet/archive_article_links.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
 from time import sleep
 
 # Open 'article_links.txt' file, archive every link that was
@@ -11,15 +10,15 @@ from time import sleep
 # regularly, because captcha and archive.is will limit your bandwidth
 with open('article_links.txt', 'r') as links:
     for link in links:
-            try:
-                browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-                browser.get('https://archive.is/?run=1&url=' + link)
-                sleep(15)
-                with open('archived_links.txt', 'a') as archive:
-                    archive.write(browser.current_url + '\n')
-                sleep(10)
-                browser.quit()
-                archive.close()
-            except:
-                pass
+        try:
+            browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+            browser.get('https://archive.is/?run=1&url=' + link)
+            sleep(15)
+            with open('archived_links.txt', 'a') as archive:
+                archive.write(browser.current_url + '\n')
+            sleep(10)
+            browser.quit()
+            archive.close()
+        except:
+            pass
 links.close()
