a95162ee683120e30e19b5ddfa4a557b7f77e30b
Gigi <dergigi@pm.me>
2023-01-22 15:58:58 +0100

    Update update_all.sh

--- scripts/spreadsheet_to_resource_mds/update_all.sh
+++ scripts/spreadsheet_to_resource_mds/update_all.sh
@@ -6,6 +6,7 @@ rm -v ../../collections/_books/*.md
 rm -v ../../collections/_episodes/*.md
 rm -v ../../collections/_podcasts/*.md
 rm -v ../../collections/_curations/*.md
+rm -v ../../collections/_categories/*.md
 
 echo "Scraping collections from spreadsheet..."
 python update_articles.py
@@ -13,3 +14,4 @@ python update_books.py
 python update_sodes.py
 python update_pods.py
 python update_curations.py
+python update_categories.py
