0b19540a50f9623d3b7a7408529939ce06de27f2
Gigi <dergigi@pm.me>
2023-01-22 16:15:44 +0100

    feat(categories): scrape script, layout

--- scripts/spreadsheet_to_resource_mds/update_categories.py
+++ scripts/spreadsheet_to_resource_mds/update_categories.py
@@ -17,16 +17,18 @@ articles = client.open("Bitcoin Resources").worksheet("Book Categories")
 for idx, row in enumerate(articles.get_all_values()):
     category_short = row[0].lstrip().rstrip()
     category_name = row[1].lstrip().rstrip()
+    category_text = row[2].lstrip().rstrip()
 
-    md_file_path = title_to_file_path(category_title, 'categories')
+    md_file_path = title_to_file_path(category_short, 'categories')
     if md_file_path == "":
         continue
 
     md_file = (
                 f"---\n"
-                f"layout: page\n"
-                f"title: {category_short}\n"
-                f"link: {category_name}\n"
+                f"layout: page-category\n"
+                f"title: {category_name}\n"
+                f"short: {category_short}\n"
+                f"text: {category_text}\n"
                 f"order: {idx}\n"
                 f"---\n")
 
